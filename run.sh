e2ae94fe931edbdeca3bfa3c3bfbfa913209b806
xuchen <xuchen_si@126.com>
2019-02-26 15:15:19 +0800

    add

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-hexo clean && hexo g && hexo s
+echo "请输入参数:"
+echo "The #1 param is clean&start up"
+echo "The #2 param is clean&deploy git"
+read -t 30 -p "please input NO:" no
+option=$no
+case $option in
+1) echo "start up" && hexo clean && hexo g && hexo s ;;
+2) echo "deploy git" && hexo clean && hexo g && hexo d ;;
+esac
+
+
