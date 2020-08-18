d7a37fd1849f1221e12a333594332a4af7f51250
abner <abner510@126.com>
2020-08-18 11:35:42 +0800

    add

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ read -t 30 -p "please input NO:" no
 option=$no
 case $option in
 
-1) echo "init object" && npm install && npm i hexo-renderer-ejs hexo-renderer-less && npm install --save hexo-deployer-git && npm install hexo-abbrlink --save && npm install hexo-generator-sitemap --save && npm install hexo-generator-baidu-sitemap --save && npm audit fix;;
+1) echo "init object" && npm install && npm i hexo-renderer-ejs hexo-renderer-less && npm i --save hexo-deployer-git && npm i hexo-abbrlink --save && npm i hexo-generator-sitemap --save && npm i hexo-generator-baidu-sitemap --save ;;
 
 2) echo "start up" && hexo clean && hexo g && hexo s ;;
 
